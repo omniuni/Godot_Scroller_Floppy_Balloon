257a02433aac86abb5126cbbe37ca0c09b36e790
Daniel M. <daniel@omniimpact.com>
2026-01-10 22:35:36 -0500

    various bug fixes

--- Scripts/ui_cursor.gd
+++ Scripts/ui_cursor.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
+@onready var sprite: Sprite2D = $CanvasLayer/Sprite2D
+
 func _ready() -> void:
-	Input.set_use_accumulated_input(false)
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	pass
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		position = event.position
+	
+func _process(_delta: float) -> void:
+	sprite.global_position = get_viewport().get_mouse_position()+(sprite.get_rect().size/2)+Vector2(8,8)
 	pass
 
 func _exit_tree():
